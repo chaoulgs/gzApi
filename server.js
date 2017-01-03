eba386bf5abf701a9325b4af64ff10a77c981daa
root <root@samir-first-droplet.localdomain>
2017-01-03 02:16:27 +0000

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 //Dependencies
 var express = require('express');
-var mongoose = require('mongoose');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
 //MongoDB
 mongoose.connect('mongodb://localhost/gzbot');
+var db = mongoose.connection;
 
-//Express
+//Express - initialize
 var app = express();
+
+app.use(cors());
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
