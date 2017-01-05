31f6b2e1012e02e0145b270a12d1533b51e51ce5
chaoulgs <chaoulgs@vcu.edu>
2017-01-05 14:11:30 -0500

    Adding the login form elements and route - progress

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,26 @@ var express = require('express');
 var router = express.Router();
 
 //Routes
+
 //register
 router.get('/register', function(req, res){
 	res.render('register');
 });
+
 //login
 router.get('/login', function(req, res){
 	res.render('login');
+});
+
+//register User
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	console.log(name);
 });
 
 //return router
