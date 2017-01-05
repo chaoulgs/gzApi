fe424d71f7ad3e02b26e10024f688c5a8bd05d25
chaoulgs <chaoulgs@vcu.edu>
2017-01-05 14:43:33 -0500

    additional includes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 //Dependencies
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
