91b033b616da40bc958caa86397be45ff923cc63
chaoulgs <chaoulgs@vcu.edu>
2017-01-05 14:34:50 -0500

    Additional login progress

--- models/user.js
+++ models/user.js
@@ -1,15 +1,58 @@
-
 //Dependencies
 var restful = require('node-restful');
 var mongoose = restful.mongoose;
+var bcrypt = require('bcryptjs');
 
 //Schema
 var userSchema = new mongoose.Schema({
-  id: Number,
-  discordId: String,
-  username: String,
-  tokens: 0
+	id: {
+		type: Number
+	},
+	discordId: {
+		type:String
+	},
+	tokens: {
+		type: Number
+	},
+	username: {
+		type: String,
+		index: true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	}
 });
 
 //Return model
 module.exports = restful.model('Users', userSchema);
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 //Routes
 
 //register
@@ -22,7 +24,36 @@ router.post('/register', function(req, res){
 	var password = req.body.password;
 	var password2 = req.body.password2;
 
-	console.log(name);
+	//validation
+	req.checkbody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register', {
+			errors: errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login.');
+		res.redirect('/users/login');
+	}
 });
 
 //return router
