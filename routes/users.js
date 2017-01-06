333a3edff94bc04f3f898f022f05e239b47f0ada
chaoulgs <chaoulgs@vcu.edu>
2017-01-06 12:08:15 -0500

    trying as it was in the example

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ passport.use(new LocalStrategy(
 ));
 
 passport.serializeUser(function(user, done){
-	done(null, user._id);
+	done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done){
